ed648c9aab272cf6811a7e4b4a4b717270976220
Roland <machodin@yandex.ru>
2023-08-08 23:00:36 +0300

    refactor

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import { getPosts } from '../controllers/posts'
+import posts from "./posts";
 
 const router = Router()
 
-router.get('/posts', getPosts)
-router.post('/posts')
+router.use(posts)
 
 export default router
